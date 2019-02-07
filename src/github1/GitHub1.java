2fd7ec228dcca09a9957c33c64ba6e4904fb3235
JDW41@students.ptcollege.edu <>
2019-02-07 11:31:23 -0500

    This is the first commit

---
+++ src/github1/GitHub1.java
@@ -0,0 +1,14 @@
+
+package github1;
+
+public class GitHub1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Wagner();
+    }
+    public static void Wagner(){
+        System.out.println("Jakob Wagner!!");
+    }
+    
+}
